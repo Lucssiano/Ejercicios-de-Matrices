4bc9a073090a36a5789c11c11f02f1b34c0e9ff7
Luciano Riente <lucianotomasriente@gmail.com>
2022-06-30 01:41:26 -0300

    Se agrega hasta el ejercicio 2

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,8 +1,8 @@
-/* 1- Dados dos valores, m (<=5) y n (<=3), ingresar por filas todos los componentes de una matriz de
+/* 1- Dados dos valores, m (<=30) y n (<=25), ingresar por filas todos los componentes de una matriz de
 m filas y n columnas. Desarrollar un programa que:
 ✓ Imprima la matriz por columna.
 ✓ Calcule e imprima el valor promedio de los componentes de la matriz.
-✓ Genere e imprima un vector donde cada componente sea la suma de la columna homóloga.
+✓ Genere e imprima un vector donde cada componente sea la suma de columna homóloga.
 ✓ Genere e imprima un vector donde cada componente sea el valor máximo de cada fila */
 
 // m (<=30) y n (<=25)
@@ -11,46 +11,59 @@ m filas y n columnas. Desarrollar un programa que:
 
 using namespace std;
 
-void cargarPorColumna(int matriz[][3], int m, int n);
-void mostrarPorColumna(int matriz[][3], int m, int n);
-float promComponentes(int matriz[][3], int m, int n);
-void sumaColumHomologa(int matriz[][3], int m, int n);
-void valorMaxFila(int matriz[][3], int m, int n);
+const int cantF = 25, cantC = 30;
+
+void inicializarMatriz(int matriz[][cantC], int m, int n);
+void cargarPorColumna(int matriz[][cantC], int m, int n);
+void mostrarPorColumna(int matriz[][cantC], int m, int n);
+float promComponentes(int matriz[][cantC], int m, int n);
+void sumaColumHomologa(int matriz[][cantC], int m, int n);
+void valorMaxFila(int matriz[][cantC], int m, int n);
 
 int main()
 {
   int m, n;
 
-  // do
-  // {
-  //    cout << "Ingrese un valor menor o igual a 5: ";
-  //   cin >> m;
-  //   cout << "Ingrese un valor menor o igual a 3: ";
-  //   cin >> n;
-  // } while (m > 5 || n > 3);
+  do
+  {
+    cout << "Ingrese un valor menor o igual a 30: ";
+    cin >> m;
+    cout << "Ingrese un valor menor o igual a 25: ";
+    cin >> n;
+  } while (m > cantC || n > cantF);
 
-  int matriz[2][3];
+  int matriz[cantF][cantC];
 
-  cargarPorColumna(matriz, 2, 3);
-  mostrarPorColumna(matriz, 2, 3);
-  cout << "El valor promedio de los componentes de la matriz es: " << promComponentes(matriz, 2, 3) << endl;
-  sumaColumHomologa(matriz, 2, 3);
-  valorMaxFila(matriz, 2, 3);
+  inicializarMatriz(matriz, cantF, cantC);
+  cargarPorColumna(matriz, m, n);
+  mostrarPorColumna(matriz, cantF, cantC);
+  cout << "El valor promedio de los componentes de la matriz es: " << promComponentes(matriz, m, n) << endl;
+  sumaColumHomologa(matriz, m, n);
+  valorMaxFila(matriz, m, n);
 
   return 0;
 }
 
-void cargarPorColumna(int matriz[][3], int m, int n)
+void inicializarMatriz(int matriz[][cantC], int m, int n)
+{
+  for (int c = 0; c < n; c++)
+    for (int f = 0; f < m; f++)
+      matriz[f][c] = 0;
+}
+
+void cargarPorColumna(int matriz[][cantC], int m, int n)
 {
   for (int c = 0; c < n; c++)
     for (int f = 0; f < m; f++)
     {
+      int num;
       cout << "Fila " << f << " columna " << c << endl;
-      cin >> matriz[f][c];
+      cin >> num;
+      matriz[f][c] = num;
     }
 }
 
-void mostrarPorColumna(int matriz[][3], int m, int n)
+void mostrarPorColumna(int matriz[][cantC], int m, int n)
 {
   cout << "Matriz:" << endl;
   for (int c = 0; c < n; c++)
@@ -61,7 +74,7 @@ void mostrarPorColumna(int matriz[][3], int m, int n)
   }
 }
 
-float promComponentes(int matriz[][3], int m, int n)
+float promComponentes(int matriz[][cantC], int m, int n)
 {
   int suma = 0;
   float prom = 0;
@@ -74,7 +87,7 @@ float promComponentes(int matriz[][3], int m, int n)
   return prom;
 }
 
-void sumaColumHomologa(int matriz[][3], int m, int n)
+void sumaColumHomologa(int matriz[][cantC], int m, int n)
 {
   int vecColum[n];
   for (int c = 0; c < n; c++)
@@ -89,7 +102,7 @@ void sumaColumHomologa(int matriz[][3], int m, int n)
     cout << "La suma de la columna homologa " << i + 1 << " es: " << vecColum[i] << endl;
 }
 
-void valorMaxFila(int matriz[][3], int m, int n)
+void valorMaxFila(int matriz[][cantC], int m, int n)
 {
   int vecMaxFila[m];
   for (int i = 0; i < m; i++)
--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -7,8 +7,116 @@ secundaria según de cuál resulte mayor la sumatoria de elementos.
 
 using namespace std;
 
+const int cantF = 25, cantC = 25;
+
+void inicializarMatriz(int matriz[][cantC], int m, int n);
+void cargarPorFila(int matriz[][cantC], int m, int n);
+void mostrarPorFila(int matriz[][cantC], int m, int n);
+void imprimirDiagonal(int matriz[][cantC], int n);
+void recorrerDiagonalPrincipal(int matriz[][cantC], int n, int filas, int columnas, int sumaDiagonal);
+void recorrerDiagonalSecundaria(int matriz[][cantC], int n, int filas, int columnas, int sumaDiagonal);
+
 int main()
 {
+  int n, n2;
+
+  do
+  {
+    cout << "Ingrese un valor menor o igual a 25: ";
+    cin >> n;
+    n2 = n;
+  } while (n > cantF);
+
+  int matriz[cantF][cantC];
+
+  inicializarMatriz(matriz, cantF, cantC);
+  cargarPorFila(matriz, n, n2);
+  mostrarPorFila(matriz, cantF, cantC);
+  imprimirDiagonal(matriz, n);
 
   return 0;
+}
+
+void inicializarMatriz(int matriz[][cantC], int m, int n)
+{
+  for (int c = 0; c < n; c++)
+    for (int f = 0; f < m; f++)
+      matriz[f][c] = 0;
+}
+
+void cargarPorFila(int matriz[][cantC], int m, int n)
+{
+  for (int f = 0; f < m; f++)
+    for (int c = 0; c < n; c++)
+    {
+      int num;
+      cout << "Fila " << f << " columna " << c << endl;
+      cin >> num;
+      matriz[f][c] = num;
+    }
+}
+
+void mostrarPorFila(int matriz[][cantC], int m, int n)
+{
+  cout << "Matriz:" << endl;
+  for (int f = 0; f < m; f++)
+  {
+    for (int c = 0; c < n; c++)
+      cout << matriz[f][c] << "   ";
+    cout << endl;
+  }
+}
+
+void imprimirDiagonal(int matriz[][cantC], int n)
+{
+  int sumaDiagP = 0, sumaDiagS = 0, contFilas = 0, contColumnas = 0;
+
+  for (contColumnas; contColumnas < n; contColumnas++)
+  {
+    sumaDiagP += matriz[contFilas][contColumnas];
+    contFilas++;
+  }
+
+  contFilas = 0;
+
+  for (contColumnas = n - 1; contColumnas >= 0; contColumnas--)
+  {
+    sumaDiagS += matriz[contFilas][contColumnas];
+    contFilas++;
+  }
+
+  if (sumaDiagP > sumaDiagS)
+    recorrerDiagonalPrincipal(matriz, n, contFilas, contColumnas, sumaDiagP);
+  else
+  {
+    if (sumaDiagP == sumaDiagS)
+    {
+      recorrerDiagonalPrincipal(matriz, n, contFilas, contColumnas, sumaDiagP);
+      recorrerDiagonalSecundaria(matriz, n, contFilas, contColumnas, sumaDiagS);
+    }
+    else
+      recorrerDiagonalSecundaria(matriz, n, contFilas, contColumnas, sumaDiagS);
+  }
+}
+
+void recorrerDiagonalPrincipal(int matriz[][cantC], int n, int filas, int columnas, int sumaDiagonal)
+{
+  filas = 0;
+  for (columnas = 0; columnas < n; columnas++)
+  {
+    cout << "Elementos de la diagonal principal: " << matriz[filas][columnas] << "  " << endl;
+    filas++;
+  }
+  cout << "Los elementos de la diagonal principal suman: " << sumaDiagonal << endl;
+}
+
+void recorrerDiagonalSecundaria(int matriz[][cantC], int n, int filas, int columnas, int sumaDiagonal)
+{
+  filas = 0;
+  for (columnas = n - 1; columnas >= 0; columnas--)
+  {
+    cout << "Elementos de la diagonal secundaria: " << matriz[filas][columnas] << "  " << endl;
+    filas++;
+  }
+  cout << "Los elementos de la diagonal secundaria suman: " << sumaDiagonal;
 }
